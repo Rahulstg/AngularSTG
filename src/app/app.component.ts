8e8362c164fa9313fdc5dd88618504498211a312
Rahulstg <136368606+Rahulstg@users.noreply.github.com>
2023-06-14 17:11:29 +0530

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularSTG_project';
-}
+  secondVariable='welcome to angular';
+  getData(){
+return this.secondVariable;
+  }
+  obj={
+      name:'testnanmeobj',
+      age:29
+  }
+  ay=['test1','test2','test3'];
+  sys=window.location.host;
+  
+  }
+ 
+
